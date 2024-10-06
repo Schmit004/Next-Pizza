c44e7cbf4947211d80948536f340e6ad3b762c54
Schmitt <twitschmit@gmail.com>
2024-10-06 16:56:19 +0300

    Связывает слайдер с полями ввода цен

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -12,9 +12,19 @@ interface Props {
   className?: string;
 }
 
+interface PriceProps {
+  priceFrom: number;
+  priceTo: number;
+}
+
 export const Filters: React.FC<Props> = ({ className }) => {
   const { ingredients, loading } = useIngredients();
   const items = ingredients.map((item) => ({ value: String(item.id), text: item.name }));
+  const [ prices, setPrices] = React.useState<PriceProps>({ priceFrom: 0, priceTo: 1000 });
+
+  const updatePrices = (name: keyof PriceProps, value: number) => {
+    setPrices({ ...prices, [name]: value });
+  };
 
   return (
     <div className={className}>
@@ -28,11 +38,31 @@ export const Filters: React.FC<Props> = ({ className }) => {
       <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
         <p className='font-bold mb-3'>Цена от и до:</p>
         <div className="flex gap-3 mb-5">
-          <Input type="number" placeholder="0" min={0} max={1000} defaultValue={0} />
-          <Input type="number" placeholder="1000" min={100} max={1000} />
+          <Input
+            type="number"
+            placeholder="0"
+            min={0}
+            max={1000}
+            value={String(prices.priceFrom)}
+            onChange={(evt) => updatePrices('priceFrom', Number(evt.target.value))}
+          />
+          <Input
+            type="number"
+            placeholder="0"
+            min={0}
+            max={1000}
+            value={String(prices.priceTo)}
+            onChange={(evt) => updatePrices('priceTo', Number(evt.target.value))}
+          />
         </div>
 
-        <RangeSlider min={0} max={1000} step={10} value={[0, 1000]} />
+        <RangeSlider
+          min={0}
+          max={1000}
+          step={10}
+          value={[prices.priceFrom, prices.priceTo]}
+          onValueChange={([priceFrom, priceTo]) => setPrices({ priceFrom, priceTo})}
+        />
       </div>
 
       <CheckboxFiltersGroup
