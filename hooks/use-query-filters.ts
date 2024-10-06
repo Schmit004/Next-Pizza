8c00f2910efcf46a7b9cc6a1c8984b3086727dc3
Schmitt <twitschmit@gmail.com>
2024-10-06 17:28:33 +0300

    Создаёт кастомный хук useQueryFilters

---
+++ hooks/use-query-filters.ts
@@ -0,0 +1,32 @@
+import React from 'react';
+import qs from 'qs';
+import { Filters } from './use-filters';
+import { useRouter } from 'next/navigation';
+
+export const useQueryFilters = (filters: Filters) => {
+  const isMounted = React.useRef(false);
+  const router = useRouter();
+
+  React.useEffect(() => {
+    if (isMounted.current) {
+      const params = {
+        ...filters.prices,
+        pizzaTypes: Array.from(filters.pizzaTypes),
+        sizes: Array.from(filters.sizes),
+        ingredients: Array.from(filters.selectedIngredients),
+      };
+
+      const query = qs.stringify(params, {
+        arrayFormat: 'comma',
+      });
+
+      router.push(`?${query}`, {
+        scroll: false,
+      });
+
+      console.log(filters, 999);
+    }
+
+    isMounted.current = true;
+  }, [filters, router]);
+};
