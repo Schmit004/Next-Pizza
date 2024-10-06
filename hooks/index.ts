6221d7e4dd98ccbdc46ce7533546b56309986bc2
Schmitt <twitschmit@gmail.com>
2024-10-06 17:22:42 +0300

    Создаёт кастомный хук useFilters

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,1 +1,2 @@
 export { useIngredients } from './use-ingredients';
+export { useFilters } from './use-filters';
---
+++ hooks/use-filters.ts
@@ -0,0 +1,69 @@
+import React from 'react';
+import { useSet } from 'react-use';
+import { useSearchParams } from 'next/navigation';
+
+interface PriceProps {
+  priceFrom?: number;
+  priceTo?: number;
+}
+
+interface QueryFilters extends PriceProps {
+  pizzaTypes: string;
+  sizes: string;
+  ingredients: string;
+}
+
+export interface Filters {
+  sizes: Set<string>;
+  pizzaTypes: Set<string>;
+  prices: PriceProps;
+  selectedIngredients: Set<string>;
+}
+
+interface ReturnProps extends Filters {
+  setPizzaTypes: (value: string) => void;
+  setSizes: (value: string) => void;
+  setPrices: (name: keyof PriceProps, value: number) => void;
+  setSelectedIngredients: (value: string) => void;
+}
+
+export const useFilters = (): ReturnProps => {
+  const searchParams = useSearchParams() as unknown as Map<keyof QueryFilters, string>;
+
+  const [pizzaTypes, { toggle: togglePizzaTypes }] = useSet(
+    new Set<string>(
+      searchParams.has('pizzaTypes') ? searchParams.get('pizzaTypes')?.split(',') : [],
+    ),
+  );
+
+  const [sizes, { toggle: toggleSizes }] = useSet(
+    new Set<string>(searchParams.has('sizes') ? searchParams.get('sizes')?.split(',') : []),
+  );
+
+  const [prices, setPrices] = React.useState<PriceProps>({
+    priceFrom: Number(searchParams.get('priceFrom')) || undefined,
+    priceTo: Number(searchParams.get('priceTo')) || undefined,
+  });
+
+  const [selectedIngredients, { toggle: toggleIngredients }] = useSet(
+    new Set<string>(searchParams.get('ingredients')?.split(',')),
+  );
+
+  const updatePrice = (name: keyof PriceProps, value: number) => {
+    setPrices((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return React.useMemo(
+    () => ({
+      pizzaTypes,
+      sizes,
+      prices,
+      selectedIngredients,
+      setPrices: updatePrice,
+      setPizzaTypes: togglePizzaTypes,
+      setSizes: toggleSizes,
+      setSelectedIngredients: toggleIngredients,
+    }),
+    [pizzaTypes, sizes, prices, selectedIngredients, togglePizzaTypes, toggleSizes, toggleIngredients],
+  );
+};
