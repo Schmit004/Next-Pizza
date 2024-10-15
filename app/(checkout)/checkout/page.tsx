3f39f6e8c12ddfabdde1dcae818f76dc734b5079
Schmitt <twitschmit@gmail.com>
2024-10-15 20:37:07 +0300

    Разделяет роуты на группы

---
+++ app/(checkout)/checkout/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+import { Container, Title } from '@/components/shared';
+
+export default function CheckoutPage() {
+
+  return (
+    <Container className="mt-10">
+      <Title text="Оформление заказа" className="font-extrabold mb-8 text-[36px]" />
+    </Container>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css';
 import { Nunito } from 'next/font/google';
-import { Header } from '@/components/shared/';
 
 const nunito = Nunito({
   subsets: ['cyrillic'],
@@ -8,18 +7,13 @@ const nunito = Nunito({
   weight: ['400', '500', '600', '700', '800', '900'],
 });
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ru">
       <head>
         <link data-rh="true" rel="icon" href="/logo.png" />
       </head>
       <body className={nunito.className}>
-        <Header />
         {children}
       </body>
     </html>
