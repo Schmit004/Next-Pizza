65bba3914122b28afd167d6a1ff4799c5229694a
Schmitt <twitschmit@gmail.com>
2024-10-17 21:27:08 +0300

    Создаёт кастомный хук usePizzaOptions

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,3 +1,4 @@
 export { useIngredients } from './use-ingredients';
 export { useFilters } from './use-filters';
+export { usePizzaOptions } from './use-pizza-options';
 export { useQueryFilters } from './use-query-filters';
