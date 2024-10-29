5f7a49c5068fa047a5414be86373cca9ae476cf8
Schmitt <twitschmit@gmail.com>
2024-10-29 20:25:14 +0300

    Создаёт хук useCart

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,3 +1,4 @@
+export { useCart } from './use-cart';
 export { useIngredients } from './use-ingredients';
 export { useFilters } from './use-filters';
 export { usePizzaOptions } from './use-pizza-options';
