cd14bda07325e127b8be97bc721c6705dace326a
Schmitt <twitschmit@gmail.com>
2024-11-30 00:58:09 +0300

    Добавляет в проект функции рассылки писем и оплаты заказа

---
+++ lib/create-payment.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { PaymentData } from '@/@types/yookassa';
+
+interface Props {
+  description: string;
+  orderId: number;
+  amount: number;
+}
+
+const PAYMENT_API_URL = 'https://api.yookassa.ru/v3/payments';
+
+export async function createPayment(details: Props) {
+  const { data } = await axios.post<PaymentData>(
+    PAYMENT_API_URL,
+    {
+      amount: {
+        value: details.amount.toString(),
+        currency: 'RUB',
+      },
+      capture: true,
+      description: details.description,
+      metadata: {
+        order_id: details.orderId,
+      },
+      confirmation: {
+        type: 'redirect',
+        return_url: process.env.YOOKASSA_CALLBACK_URL,
+      },
+    },
+    {
+      auth: {
+        username: process.env.YOOKASSA_STORE_ID as string,
+        password: process.env.YOOKASSA_API_KEY as string,
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'Idempotence-Key': Math.random().toString(36).substring(7),
+      },
+    },
+  );
+
+  return data;
+}
